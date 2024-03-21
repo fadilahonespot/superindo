d5c59fd09847a7ffddbc98ba1b935ed5a1d16aff
Ahmad Fadilah <ahmad@akasia.id>
2024-03-21 14:13:50 +0700

    add seeder transaction

--- utils/database/seeder.go
+++ utils/database/seeder.go
@@ -3,7 +3,6 @@ package database
 import (
 	"log"
 	"os"
-	"time"
 
 	"github.com/fadilahonespot/superindo/entity"
 	"gorm.io/gorm"
@@ -11,37 +10,44 @@ import (
 
 func Seeder(db *gorm.DB) *gorm.DB {
 	var categoryCount int64
+	tx := db.Begin()
 	err := db.Model(&entity.Category{}).Count(&categoryCount).Error
 	if err != nil {
+		tx.Rollback()
 		panic(err)
 	}
 	if categoryCount == 0 {
 		sqlFile, err := os.ReadFile("./resources/categories.sql")
 		if err != nil {
+			tx.Rollback()
 			log.Fatal(err)
 		}
-		db = db.Exec(string(sqlFile))
+		db = tx.Exec(string(sqlFile))
 		if err != nil {
+			tx.Rollback()
 			log.Fatal(err)
 		}
-		time.Sleep(time.Duration(4) * time.Second)
 	}
 
 	var productCount int64
 	err = db.Model(&entity.Product{}).Count(&productCount).Error
 	if err != nil {
+		tx.Rollback()
 		panic(err)
 	}
 	if productCount == 0 {
 		sqlFile, err := os.ReadFile("./resources/products.sql")
 		if err != nil {
+			tx.Rollback()
 			log.Fatal(err)
 		}
-		db = db.Exec(string(sqlFile))
+		db = tx.Exec(string(sqlFile))
 		if err != nil {
+			tx.Rollback()
 			log.Fatal(err)
 		}
 	}
 
+	tx.Commit()
 	return db
 }
